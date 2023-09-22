ce60115dfae459f4a030e6abd8a6e258529f387c
Notand105 <urzua.fernando.f@gmail.com>
2023-09-22 12:25:27 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { headerLogo } from '../assets/images/header-logo.svg'
+import { hamburger } from '../assets/icons'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <a href="/">
+            <img src='../assets/images/header-logo.svg' alt='header logo' />
+        </a>
+    </nav>
+  )
+}
+
+export default Navbar
